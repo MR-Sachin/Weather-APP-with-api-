079b3bfdce5d45abdd8052eff0dac6cb0ab8fc42
Sachinverma <sacv21@gmail.com>
2021-04-04 11:34:18 +0530

    Initial Commit

---
+++ Weather-APP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather-APP
+//
+//  Created by sachiin verma on 04/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather-APP/Weather_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_APPApp.swift
+//  Weather-APP
+//
+//  Created by sachiin verma on 04/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Weather_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
