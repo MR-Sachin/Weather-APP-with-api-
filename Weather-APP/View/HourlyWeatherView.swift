ec7ab84a04d6077721c9976327c6da853dee873e
Sachinverma <sacv21@gmail.com>
2021-04-05 19:07:52 +0530

    Create Hourly Weather View

---
+++ Weather-APP/View/HourlyWeatherView.swift
@@ -0,0 +1,51 @@
+//
+//  HourlyWeatherView.swift
+//  Weather-APP
+//
+//  Created by sachiin verma on 05/04/21.
+//
+
+import SwiftUI
+
+struct HourlyWeatherView: View {
+    @ObservedObject var cityVM: CityViewViewModel
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing:20) {
+                ForEach(cityVM.weather.hourly) { weather in
+                    let icon = cityVM.getWeatherIconFor(icon: weather.weather.count > 0 ? weather.weather[0].icon : "sun.max.fill")
+                    let hour = cityVM.getTimeFor(timestamp: weather.dt)
+                    let temp = cityVM.getTempFor(temp: weather.temp)
+                    
+                    getHourlyView(hour: hour, image: icon, temp: temp)
+                    
+                    
+                    
+                }
+            }
+        }
+    }
+
+
+
+private func getHourlyView(hour: String, image: Image, temp: String) -> some View {
+    
+    VStack(spacing:20) {
+        Text(hour)
+        image
+            .foregroundColor(.yellow)
+    }
+    .foregroundColor(.white)
+    .padding()
+    .background(RoundedRectangle(cornerRadius: 5).fill(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))]), startPoint: .topLeading, endPoint: .bottomTrailing)))
+    .shadow(color: Color.white.opacity(0.1), radius: 2, x: -2, y: -2)
+    .shadow(color: Color.black.opacity(0.2), radius: 2, x: 2, y: 2)
+}
+}
+    
+struct HourlyWeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+    
